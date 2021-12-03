a212a682eb2b0ca944f37db6cc72190a02a3a6b5
luca-Serena <lucaserena35@gmail.com>
2021-12-03 15:34:17 +0100

    initial commit

---
+++ entity_definition.h
@@ -0,0 +1,49 @@
+/*	##############################################################################################
+ *      Advanced RTI System, ARTÌS			http://pads.cs.unibo.it
+ *      Large Unstructured NEtwork Simulator (LUNES)
+ *
+ *      Description:
+ *              -	In this file is defined the state of the simulated entitiesFORKI
+ *
+ *      Authors:
+ *              First version by Gabriele D'Angelo <g.dangelo@unibo.it>
+ *
+ ############################################################################################### */
+
+#ifndef __ENTITY_DEFINITION_H
+#define __ENTITY_DEFINITION_H
+//#define HIERARCHY
+
+#include "lunes_constants.h"
+
+
+/*---- E N T I T I E S    D E F I N I T I O N ---------------------------------*/
+
+
+/*! \brief Records composing the local state (dynamic part) of each SE
+ *         NOTE: no duplicated keys are allowed
+ */
+struct state_element {
+    unsigned int  key;      // Key
+    unsigned int value; // Value
+};
+
+typedef struct cache_element{
+    unsigned int id;
+    unsigned int timestamp;
+} cache_element;
+
+/*! \brief SE state definition */
+typedef struct hash_data_t {
+    int           key;                  // SE identifier
+    int           lp;                   // Logical Process ID (that is the SE container)
+    int           internal_timer;       // Used to track mining activity
+    int	   status;		  // 0 off 1 active 2 applicant 3 holder 
+    GHashTable *  state;                // Local state as an hash table (glib) (dynamic part)
+    int	  received;	  	  // 0 not received anything, !=0 received the message, -1 received the message back in the fluff phase
+    unsigned int  num_neighbors;        // Number of SE's neighbors (dynamically updated)
+    int	   buffer [50];
+    cache_element cache [20];		
+} hash_data_t;
+
+#endif /* __ENTITY_DEFINITION_H */
---
+++ lunes.h
@@ -0,0 +1,34 @@
+/*	##############################################################################################
+ *      Advanced RTI System, ARTÌS			http://pads.cs.unibo.it
+ *      Large Unstructured NEtwork Simulator (LUNES)
+ *
+ *      Description:
+ *              -	Function prototypes
+ *
+ *      Authors:
+ *              First version by Gabriele D'Angelo <g.dangelo@unibo.it>
+ *
+ ############################################################################################### */
+
+#ifndef __LUNES_H
+#define __LUNES_H
+
+
+#include "utils.h"
+#include "entity_definition.h"
+
+
+void lunes_real_forward(hash_node_t *, Msg *, unsigned short, cache_element, int, unsigned int, unsigned int);
+void lunes_forward_to_neighbors(hash_node_t *, Msg *, unsigned short, cache_element, int, unsigned int, unsigned int);
+
+
+// LUNES handlers
+void lunes_user_request_event_handler(hash_node_t *, int, Msg *);
+void lunes_user_item_event_handler(hash_node_t *, int, Msg *);
+void lunes_user_register_event_handler(hash_node_t *);
+void lunes_user_control_handler(hash_node_t *);
+
+// Support functions
+int cache_choose_oldest(hash_node_t*);
+
+#endif /* __LUNES_H */
